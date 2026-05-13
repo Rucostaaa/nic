6a071040618d0b2221da0da24a4734ea0938a1b7
Ruben <rubenfilipecosta.maths@gmail.com>
2026-05-13 04:45:54 -0700

    Your commit message here

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL, // production frontend (Netlify/Vercel)
+    ],
+    credentials: true,
+  }));
 
 // Routes
 app.use('/api/auth',express.json(), authRoutes);
@@ -22,19 +28,13 @@ app.use('/api/statics', staticsRoutes);
 app.use('/api/admin',express.json(), adminRoutes);
 app.use('/api/twilio',express.json(), twilioRoutes);
 
+console.log("Mongo",process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI)
-
   .then(() => {
     console.log('MongoDB connected');
-
- app.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log(`Server running on port : ${process.env.PORT}`);
-});
-
+    app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+      console.log(`Server running on port : ${process.env.PORT || 5000}`);
+    });
   })
-  .catch(err => {
-    console.log('MongoDB connection error:', err);
-  });
-
-
+  .catch(err => console.log('MongoDB connection error:', err));
